21a8e870fd479eb76211a5c74bd80c90699f6c0f
Sonlouis <sonnavi94@gmail.com>
2023-12-21 21:48:26 +0700

    [Add] Krshop Project

---
+++ src/test/java/com/vti/KrShop/KrShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.KrShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
